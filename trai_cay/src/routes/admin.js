4cd29490f2003decd1ea04b7ab8eddaa7539fbfe
hiquoc <dangquochuy26122003@gmail.com>
2025-03-09 14:29:35 +0700

    Deleted folder

--- trai_cay/src/routes/admin.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router=express.Router();
-let adminController = require('../app/controllers/adminController')
-
-// router.get('/check', adminController.check); 
-router.get('/account', adminController.account); 
-
-
-module.exports = router;
--- trai_cay/src/routes/main.js
+++
@@ -1,9 +0,0 @@
-const express = require('express');
-const router=express.Router();
-let mainController = require('../app/controllers/mainController')
-
-router.post('/logout',mainController.logout)
-router.get('/', mainController.main);
-
-
-module.exports = router;
